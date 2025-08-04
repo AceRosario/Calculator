ccae6d5d57cf25b090b399866f8c35403cb84eed
ace <haroldace12345@gmail.com>
2025-08-03 21:01:24 -0700

    tried my best. cant problem solve as well

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-function calculate(x,y){
+const display = document.getElementById("display");
 
+function append(input) {
+    display.value += input;
+}
 
+function clear() {
+    display.value = "";
+}
 
+function calculate() {
+    
+    display.value = eval(display.value);
+    
     
 }
